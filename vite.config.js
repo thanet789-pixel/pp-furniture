a4dc22c82fc4ebb5a00fb7571ec3646cf537f287
Thanet Pongsaka <thanet789@gmail.com>
2026-05-20 12:43:50 +0700

    premium update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+import fs from 'fs'
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const projectRoot = fs.realpathSync(process.cwd())
+
 // https://vite.dev/config/
 export default defineConfig({
+  root: projectRoot,
+  base: './',
+  resolve: {
+    preserveSymlinks: true,
+  },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: path.resolve(projectRoot, 'index.html'),
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  }
 })
